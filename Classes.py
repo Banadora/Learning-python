c175c4cc157f1b83743c017e1432d42de5d8617d
Banadora <aoi-@hotmail.fr>
2017-03-28 19:13:29 +0200

    Remise en place de l'interface

--- Classes.py
+++ Classes.py
@@ -15,14 +15,15 @@ class MakeUI(Frame):
     def __init__(self, Fenetre, **kwargs):
 
         # Héritage et données
-        Frame.__init__(self, Fenetre, width=1500, height=576, **kwargs)
+        Frame.__init__(self, Fenetre, width=1500, height=700, **kwargs)
 
         self.pack(fill=BOTH)
-        self.InfosTouches = "Vous êtes un ~ qui cherche à s'échapper du labyrinthe pour gagner du $$$\n\n \
-                            Tapez Z pour aller vers le haut\n \
-                            Tapez Q pour aller vers la gauche\n \
-                            Tapez D pour aller vers la droite\n \
-                            Tapez S pour aller vers le bas\n"
+        self.InfosTouches = "\nVous êtes un héros qui cherche à s'échapper \ndu labyrinthe pour gagner du $$$\n\n" + \
+         " Appuyez Z pour aller vers le haut\n" + \
+         "   Appuyez Q pour aller vers la gauche\n" \
+         "   Appuyez D pour aller vers la droite\n" \
+         "Appuyez S pour aller vers le bas\n\n" + \
+        "---------------------------------------------------------\n\n"
         self.FileName = ""
         self.CarteActuelle = {}
         self.ListeLignes = []
@@ -57,11 +58,11 @@ class MakeUI(Frame):
 
         # Cadre d'affichage de l'aancienne carte et des infos de jeu
         self.FrameCarte = LabelFrame(self.FrameGeneral, text="Infos", padx=2, pady=2)
-        self.FrameCarte.pack(side=LEFT, fill="both", expand="yes", padx=5, pady=5)
+        self.FrameCarte.pack(side=LEFT, fill="both", expand="yes", padx=2, pady=2)
 
         # Cadre d'affichage de la carte graphique
         self.FrameCarteGraph = LabelFrame(self.FrameGeneral, text="Carte", padx=2, pady=2)
-        self.FrameCarteGraph.pack(side=RIGHT, fill="both", expand="yes", padx=5, pady=5)
+        self.FrameCarteGraph.pack(side=RIGHT, fill="both", expand="yes", padx=2, pady=2)
 
         # Cadre des boutons de commandes
         self.FrameCtrl = Frame(self, padx=2, pady=2)
@@ -71,28 +72,25 @@ class MakeUI(Frame):
         self.MsgInfos = Label(self.FrameInfos, text="Sélection du personnage et de la carte")
         self.MsgInfos.pack(side="left", padx=5, pady=5)
 
+        # Bouton pour quitter le programme
+        self.BtnQuitter = Button(self.FrameInfos, text="Quitter", bg="red", width=10, command=self.quit)
+        self.BtnQuitter.pack(side="right", padx=5, pady=5)
+
         # Bouton d'ouverture de carte
         self.BtnOuvrirCarte = Button(self.FrameInfos, text="Carte", bg="orange", state=DISABLED, width=10, command=self.OuvrirCarte)
         self.BtnOuvrirCarte.pack(side="right", padx=5, pady=5)
 
         # Bouton de choix du personnage
-        self.BtnChoixPerso = Button(self.FrameInfos, text="Personnage", bg="orange", width=10, command=self.ChoisirPerso)
+        self.BtnChoixPerso = Button(self.FrameInfos, text="Personnage", bg="orange", state=NORMAL, width=10, command=self.ChoisirPerso)
         self.BtnChoixPerso.pack(side="right", padx=5, pady=5)
 
-        # Bouton pour quitter le programme
-        self.BtnQuitter = Button(self.FrameCtrl, text="Quitter", bg="red", width=20, command=self.quit)
-        self.BtnQuitter.pack(side="left", padx=5, pady=5)
-
-        self.AfficherTouche = Label(self.FrameCtrl, text="None",)
-        self.AfficherTouche.pack(side="right", padx=5, pady=5)
-
-        self.MsgInfosCarte = Label(self.FrameCarte, text=self.InfosTouches, width=60)
+        self.MsgInfosCarte = Label(self.FrameCarte, text="", width=45)
         self.MsgInfosCarte.pack(padx=2, pady=2)
 
-        self.Carte = Label(self.FrameCarte, text=" ")
-        self.Carte.pack(padx=2, pady=2)
+        self.MsgInfosCarteSup = Label(self.FrameCarte, text="", font=('Lucida Console', 10, 'bold'), width=45)
+        self.MsgInfosCarteSup.pack(padx=2, pady=2)
 
-        self.CanvasCarte = Canvas(self.FrameCarteGraph, width=680, height=680)
+        self.CanvasCarte = Canvas(self.FrameCarteGraph, width=670, height=660)
         self.CanvasCarte.pack(padx=0, pady=0)
 
 ###################################################################################################################
@@ -113,15 +111,12 @@ class MakeUI(Frame):
         self.ListeLignes = []
         self.CarteActuelle = {}
         self.FileName = ""
+        self.MsgInfosCarte["text"] = ""
 
         self.FileName = askopenfilename(title="Ouvrir une carte", filetypes=[('txt files', '.txt'), ('all files', '.*')])
         self.Fichier = open(self.FileName, "r")
         self.CarteActuelle = self.Fichier.read()
         self.Fichier.close()
-
-        self.FinDeCarte = 0
-        self.Carte["text"] = self.CarteActuelle
-        self.Carte["font"] = ('Lucida Console', 12, 'bold')
 
         # Y = Ligne   X = Colonne
         if self.FileName[-5] == '1':
@@ -137,6 +132,9 @@ class MakeUI(Frame):
             self.NbColonnes = 20
             self.FrameCarteGraph["text"] = " 2 : Petite caverne  (Moyen)"
 
+        self.FinDeCarte = 0
+        self.MsgInfosCarte["font"] = ('Lucida Console', 8)
+
         i = 0
         j = 0
         k = self.NbColonnes
@@ -147,8 +145,9 @@ class MakeUI(Frame):
             k = j + self.NbColonnes
 
         self.MsgInfosCarte["text"] = ""
-        self.MsgInfos["text"] = "\n Position >>   Ligne : "\
-             + str(self.PosY + 1) + "   Colonne : " + str(self.PosX)
+        self.MsgInfosCarte["text"] = self.InfosTouches + "\n Position >>   Ligne : "\
+             + str(self.PosY + 1) + "   Colonne : " + str(self.PosX) + \
+        "\n\n\n---------------------------------------------------------\n\n"
 
 ######################################################
         # Suppression de l'ancienne carte si necessaire
@@ -169,8 +168,6 @@ class MakeUI(Frame):
 ###################################################################################################################
 ###################################################################################################################
     def Play(self, Touche):
-        self.AfficherTouche["text"] = Touche
-
         if self.FinDeCarte == 1:
             TouchePressee = "NULL"
         else:
@@ -183,7 +180,7 @@ class MakeUI(Frame):
             LigneDuHero = self.ListeLignes[self.PosY]
             NouvelleLigneDuHero = self.ListeLignes[self.PosY - 1]
             if NouvelleLigneDuHero[self.PosX - 1:self.PosX] == '#':
-                InfoSup = "Il est impossible de traverser un mur !! Bien essayé."
+                InfoSup = "\n\nIl est impossible de traverser un mur !! \nBien essayé."
             elif NouvelleLigneDuHero[self.PosX - 1:self.PosX] == '$':
                 self.FinDeCarte = 1
             else:
@@ -207,7 +204,7 @@ class MakeUI(Frame):
         elif TouchePressee == "Q":
             LigneDuHero = self.ListeLignes[self.PosY]
             if LigneDuHero[self.PosX - 2:self.PosX - 1] == '#':
-                InfoSup = "Il est impossible de traverser un mur !! Bien essayé."
+                InfoSup = "\n\nIl est impossible de traverser un mur !! \nBien essayé."
             elif LigneDuHero[self.PosX - 2:self.PosX - 1] == '$':
                 self.FinDeCarte = 1
             else:
@@ -226,7 +223,7 @@ class MakeUI(Frame):
             LigneDuHero = self.ListeLignes[self.PosY]
             NouvelleLigneDuHero = self.ListeLignes[self.PosY + 1]
             if NouvelleLigneDuHero[self.PosX - 1:self.PosX] == '#':
-                InfoSup = "Il est impossible de traverser un mur !! Bien essayé."
+                InfoSup = "\n\nIl est impossible de traverser un mur !! \nBien essayé."
             elif NouvelleLigneDuHero[self.PosX - 1:self.PosX] == '$':
                 self.FinDeCarte = 1
             else:
@@ -250,7 +247,7 @@ class MakeUI(Frame):
         elif TouchePressee == "D":
             LigneDuHero = self.ListeLignes[self.PosY]
             if LigneDuHero[self.PosX:self.PosX + 1] == "#":
-                InfoSup = "Il est impossible de traverser un mur !! Bien essayé."
+                InfoSup = "\n\nIl est impossible de traverser un mur !! \nBien essayé."
             elif LigneDuHero[self.PosX:self.PosX + 1] == '$':
                 self.FinDeCarte = 1
             else:
@@ -286,9 +283,15 @@ class MakeUI(Frame):
             self.CarteActuelle += "\n"
             i += 1
 
-        self.MsgInfosCarte["text"] = InfoSup
+        self.MsgInfosCarte["text"] = ""
+        self.MsgInfosCarteSup["text"] = ""
+
+        self.MsgInfosCarte["text"] = self.InfosTouches + "\n Position >>   Ligne : " + str(self.PosY + 1) + \
+            "   Colonne : " + str(self.PosX) + \
+        "\n\n\n---------------------------------------------------------\n\n"
+
         if self.FinDeCarte == 0:
-            self.Carte["text"] = self.CarteActuelle
+            self.MsgInfosCarteSup["text"] = InfoSup
         else:
-            self.Carte["text"] = self.CarteActuelle + "\nBRAVO : pensez à dépenser vos $$$ !"
-        self.MsgInfos["text"] = "\n Position >>   Ligne : " + str(self.PosY + 1) + "   Colonne : " + str(self.PosX)
+            InfoSup = "\nBRAVO : pensez à dépenser vos $$$ !"
+            self.MsgInfosCarteSup["text"] = InfoSup
